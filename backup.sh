9f16be940d5faf3beca08be19fb300fb263d1861
Sumit Debbarma <sumitdebbarma1433@gmail.com>
2026-02-06 00:23:17 +0530

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "**This script take backup of /etc everyday at 20:00pm*"
+
+tar -zcf /source/backup_of_tmp-$(date +%A%y%m%d%H%M%S).tar.gz /tmp 2>/dev/null
+sleep 2
+
+echo "**backup successful**"
+
+echo *********NOW SYNCING WITH REMOTE SERVER********
+
+rsync -a /source/* 172.16.0.100:/remote/dist/
+sleep 2
+
+echo "**backup and syncing both successful**"
+
+exit
+
